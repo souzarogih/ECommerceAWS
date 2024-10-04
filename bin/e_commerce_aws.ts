e33c7de060ce4389a71bad7a6d65b8b41a6a23b9
higorsouza <higor.andrade@hotmail.com>
2024-10-04 20:19:02 -0300

    subindo projeto

---
+++ bin/e_commerce_aws.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { ProductAppStack } from '../lib/productsApp-stack';
+import { ECommerceApiStack } from '../lib/ecommerceApi-stack';
+import { ProductsAppLayersStack } from '../lib/productsAppLayers-stack';
+
+const app = new cdk.App();
+
+const env: cdk.Environment = {
+  account: "767398081323",
+  region: "us-east-1"
+}
+
+const tags = {
+  cost: "ECommerce",
+  team: "miriades-ecommerce"
+}
+
+const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayers", {
+  tags: tags,
+  env: env
+})
+
+const productsAppStack = new ProductAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+})
+productsAppStack.addDependency(productsAppLayersStack)
+
+const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  productsAdminHandler: productsAppStack.productsAdminHandler,
+  tags: tags,
+  env: env
+})
+eCommerceApiStack.addDependency(productsAppStack)
